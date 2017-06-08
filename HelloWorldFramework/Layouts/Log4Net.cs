517131bd0f6baf9196baf284745c23762bc29240
prasadbvm <prasadbvm@gmail.com>
2017-06-08 17:53:08 -0500

    Add files via upload

---
+++ HelloWorldFramework/Layouts/Log4Net.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorldFramework.Layouts
+{
+    public class Log4Net
+    {
+        //Log4Net Layout for capturing and formatting a JSON string
+    }
+}
---
+++ HelloWorldFramework/WrapperClasses/IUri.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldFramework.WrapperClasses
+{
+    public interface IUri
+    {
+        Uri GetUri(string uriString);
+    }
+}
